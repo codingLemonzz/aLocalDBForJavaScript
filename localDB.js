b5ba04f1276067e5925287d65a59a9fd3de58ec6
zrygoalone <35480636+zrygoalone@users.noreply.github.com>
2018-05-21 17:12:29 +0800

    a data manage tool for javaScript

---
+++ localDB.js
@@ -0,0 +1,113 @@
+var localDB = {
+	localMap: {},
+
+<<<<<<< HEAD
+	//set data 
+	set: function(key, value) {
+		localDB.localMap[key] = value;
+	},
+	//set a batch data
+	arrSet: function(arr) {
+		if(Array.isArray(arr)){
+			for(var i=0; i<arr.length; i++){
+				if(Array.isArray(arr[i])){
+					localDB.localMap[arr[i][0]]=arr[i][1];
+				}
+			}
+		}
+	},
+	//get data according to key
+	get: function(key) {
+		return localDB.localMap[key];
+	},
+	//get a batch data 
+	arrGet: function(arr) {
+		var dataArr=new Array();
+		if(Array.isArray(arr)){
+			for(var i=0; i<arr.length; i++){
+				dataArr.push([arr[i],localDB.localMap[arr[i]]]);
+			}
+		}
+		return dataArr;
+	},
+	//get data where key contains the args
+	getLike: function(likeKey) {
+		var likeArr = new Array();
+		for(var key in localDB.localMap){
+			if(key.indexOf(likeKey) >0){
+				likeArr.push([key,localDB.localMap[key]]);
+			}
+		}
+		return likeArr;
+	},
+	//delete data according to key
+	delete: function(key) {
+		delete localDB.localMap[key];
+	},
+	//delete a batch data
+	arrDelete: function(arr) {
+		if(Array.isArray(arr)){
+			for(var i=0; i<arr.length; i++){
+				delete localDB.localMap[arr[i]];
+			}
+		}
+	},
+	//get all data 
+=======
+	set: function(key, value) {
+		localDB.localMap[key] = value;
+	},
+
+	get: function(key) {
+		return localDB.localMap[key];
+	},
+
+	delete: function(key) {
+		delete localDB.localMap[key];
+	},
+
+>>>>>>> 89428a04a97947846ba3b550f666e72760b4f673
+	getAll: function() {
+		var arr = new Array();
+		for(var key in localDB.localMap) {
+			arr.push([key, localDB.localMap[key]]);
+		}
+		return arr;
+	},
+<<<<<<< HEAD
+	//delete all data
+	deleteAll: function() {
+		localDB.localMap = {};
+	},
+	//set data where have the lifecycle
+=======
+
+	deleteAll: function() {
+		localDB.localMap = {};
+	},
+
+>>>>>>> 89428a04a97947846ba3b550f666e72760b4f673
+	minSet: function(key, value, millisecond) {
+		if(!util.checkNumber(millisecond)) {
+			return "The third args is not a number!";
+		} else {
+			localDB.localMap[key] = value;
+			setTimeout(function() {
+				delete localDB.localMap[key];
+			}, millisecond);
+		}
+	}
+	
+};
+
+var util = {
+	checkNumber: function(val) {
+		var regPos = /^\d+(\.\d+)?$/; //Nonnegative floating point number
+		var regNeg = /^(-(([0-9]+\.[0-9]*[1-9][0-9]*)|([0-9]*[1-9][0-9]*\.[0-9]+)|([0-9]*[1-9][0-9]*)))$/; //Negative floating point number
+		if(regPos.test(val) || regNeg.test(val)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
